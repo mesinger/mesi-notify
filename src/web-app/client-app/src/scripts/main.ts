aaa7f7577fe793dc321309cf56289d3de71c839a
mesi <uintptr96@gmail.com>
2021-02-14 20:55:47 +0100

    Add sass

--- src/web-app/client-app/src/scripts/main.ts
+++ src/web-app/client-app/src/scripts/main.ts
@@ -1,1 +1,4 @@
+import "bootstrap/js/dist/dropdown.js";
+import "bootstrap/js/dist/collapse.js";
+
 import "./components/execute-command-form";
--- src/web-app/client-app/webpack.config.js
+++ src/web-app/client-app/webpack.config.js
@@ -1,22 +1,45 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/scripts/main.ts',
-    mode: "production",
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, '../wwwroot/scripts'),
-    },
+  entry: {
+    main: ['./src/scripts/main.ts', './src/styles/main.scss']
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].css',
+              outputPath: 'styles'
+            }
+          },
+          {
+            loader: 'extract-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          },
+        ]
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'scripts/main.js',
+    path: path.resolve(__dirname, '../wwwroot'),
+  },
 };
